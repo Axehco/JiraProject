6b7a21779f15c8ae1cabc4173bad8df3d4e160be
Axehco <mbozhong@outlook.com>
2022-11-29 23:02:46 +0800

    添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { SearchPanel } from "screens/project-list/search-panel";
 import { List } from "screens/project-list/list";
 import qs from 'qs'
-import { cleanObject } from 'utils/index'
+import { cleanObject, useMount, useDebounce } from 'utils/index'
 
 const apiURL = process.env.REACT_APP_API_URL
 
@@ -13,25 +13,36 @@ export const ProjectListScreen = () => {
     personId: ''
   })
 
+  // 这里使用自定义的 useDebounce 这个hook
+  const debouncedParam = useDebounce(param, 2000)
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
 
   React.useEffect(() => {
     // qs.stringify会转化为name=XXX&personId=XXX &personId=${param.personId}
-    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])  // param改变的时候去获取
+  }, [debouncedParam])  // param改变的时候去获取
 
-  React.useEffect(() => {
+  // useMount用了后，就不需要这个奇怪的空数组了
+  useMount(() => {
     fetch(`${apiURL}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [param])  // param改变的时候去获取
+  })  
+
+  /* React.useEffect(() => {
+    fetch(`${apiURL}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  }, []) */
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 // 两个叹号，把一个值转化为bool值
 export const isFalse = (value) => value === 0 ? false : !value
 
@@ -12,4 +14,26 @@ export const cleanObject = (object) => {
     }
   })
   return result
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+// 自定义hook最大的特征，里面需要使用别的hook，不然普通函数就能做到了。
+export const useDebounce = (value, delay) => {
+  // debouncedValue 为定义的内部变量 debouncedValue的更新频率和上面的参数value不一样
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    // 每次value变化时，会设置一个新的定时器，delay后再去 setDebouncedValue(value)
+    const timeout = setTimeout(() => { setDebouncedValue(value) }, delay)
+
+    // 执行时间，是在上一次useEffect处理完以后再去执行。所以一般负责一些清理任务。
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue
 }
